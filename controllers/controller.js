75e5302e3ef8015c091f3ad3ef4ff8e03e248fee
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-02-12 15:43:41 +0100

    add: modify destroy's function, fine es

--- controllers/controller.js
+++ controllers/controller.js
@@ -29,30 +29,24 @@ function update(req, res) {
     res.send('Modifica integrale del post ' + req.params.id); 
 }
 function destroy(req, res) {
-    console.log("DELETE request ricevuta per il post con ID:", req.params.id); // Aggiungi questa riga per il log
-
-    const id = parseInt(req.params.id);
+    const id = parseInt(req.params.id)
+    // cerchiamo il pizza tramite id
     const post = arrPosts.find(post => post.id === id);
-
+    // Piccolo controllo
     if (!post) {
-        return res.status(404).json({
-            status: 404,
-            error: "Not Found",
-            message: "Post non trovato"
-        });
+    return res.status(404).json({
+    status: 404,
+    error: "Not Found",
+    message: "post non trovato"
+    })
     }
-
-    const postIndex = arrPosts.indexOf(post);
-
-    if (postIndex !== -1) {
-        arrPosts.splice(postIndex, 1);
-    }
-
-    console.log(arrPosts);  // Log dell'array dopo la rimozione
-
-    res.sendStatus(204);  // Restituiamo il codice di stato 204
-}
-
+    // Rimuoviamo la pizza dal menu
+    arrPosts.splice(arrPosts.indexOf(post), 1);
+    // Verifichiamo sul terminale
+    console.log(arrPosts);
+    // Restituiamo lo status corretto
+    res.sendStatus(204)
+    };
 
 // esportiamo tutto
 module.exports = { index, show, store, update, destroy }
