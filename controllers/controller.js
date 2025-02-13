118231dfba075715c49beadddab29a33a78f25d9
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-02-13 15:24:31 +0100

    add: update and text with postman, milestone 4, fine esercizio

--- controllers/controller.js
+++ controllers/controller.js
@@ -22,6 +22,11 @@ function show(req, res) {
     res.json(post);
 };
 
+
+
+
+
+
 function store(req, res) {
     //creiamo un nuovo id incrementando l'ultimo id presente
     const newId = arrPosts[arrPosts.length - 1].id + 1;
@@ -47,9 +52,51 @@ function store(req, res) {
     res.json(newPost);
 
     }
+
+
+
+
+
+
+
 function update(req, res) {
-    res.send('Modifica integrale del post ' + req.params.id); 
+    //recuperiamo l'id dall'URL e trasformiamolo in numero con il paresInt
+    const id = parseInt(req.params.id);
+    console.log("ID ricevuto:", id);
+    
+
+    //cerchiamo il post tramite id
+    const post = arrPosts.find(post => post.id === id);
+
+    //facciamo un piccolo controllo 
+    if (!post){
+        res.status(404);
+
+        return res.json({
+            error:"Not Found",
+            message:"Post non trovato"
+        })
+    }
+
+    //aggiorniamo il post
+    post.title = req.body.title;
+    post.slug = req.body.slug;
+    post.content = req.body.content;
+    post.image = req.body.image;
+    post.tags = req.body.tags;
+
+    //controlliamo l'array dei post
+    console.log(arrPosts);
+    //restituiamo il post appena aggiornato
+    res.json(post);
 }
+
+
+
+
+
+
+
 function destroy(req, res) {
     const id = parseInt(req.params.id)
     // cerchiamo il pizza tramite id
